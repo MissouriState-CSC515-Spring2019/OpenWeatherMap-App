c6bb268226c5010bfc1f6e0419ff753c68630fd1
Daniel <dwc678@live.missouristate.edu>
2019-04-17 20:15:13 -0500

    Home page, Navbar, template created
    Home page with the application title and a search bar for "zip code" have been created. Navbar with blank links have also been created. Grid system using "reacstrap" has been created.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import { 
+  Container, 
+  Row, 
+  Col, 
+  Button, 
+  Form, 
+  FormGroup, 
+  Label,
+  Input,  
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem 
+} from 'reactstrap';
 import './App.css';
+
 
 const ApiKey = '304b69dfc8fd594456d6556ba7d5be48';
 const zipcode = '65810';
 const countrycode = 'us';
-const url = 'api.openweathermap.org/data/2.5/weather?zip='+ zipcode + ',' + countrycode + '&appid=' + ApiKey;
+const url = 'https://api.openweathermap.org/data/2.5/weather?zip='+ zipcode + ',' + countrycode + '&appid=' + ApiKey;
 
 
 class MyComponent extends React.Component {
@@ -25,7 +47,7 @@ class MyComponent extends React.Component {
           console.log(result)
           this.setState({
             isLoaded: true,
-            items: result.weather
+            items: result
           });
         },
         // Note: it's important to handle errors here
@@ -48,7 +70,37 @@ class MyComponent extends React.Component {
       return <div>Loading...</div>;
     } else {
       return (
-        <p>Hello</p>
+        <Container>
+          <Navbar color="light" light expand="lg">
+            <NavbarBrand href="/">The Weather App</NavbarBrand>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/hourly/">Hourly Forecast</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/forecast/">5 Day Forecast</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/radar/">Weather Radar</NavLink>
+              </NavItem>
+            </Nav>
+          </Navbar>
+      
+          <Row>
+            <Col id="Title">The Weather App</Col>
+          </Row>
+    
+          <Row>
+            <Col sm="12" md={{ size: 6, offset: 3 }} id="ZipCode-Form">
+              <Form inline id="ZipCode-Form" >
+                <FormGroup>
+                  <Input id="ZipCode-Input" placeholder="ex. 65810"></Input>
+                  <Button type="submit"> Search </Button>
+                </FormGroup>
+              </Form>
+              </Col>
+            </Row>
+        </Container>
       );
     }
   }
