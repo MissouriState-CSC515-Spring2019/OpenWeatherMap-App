cbaf30fb860d355b5f9a6ebc67152383d84e7c9c
kpmcdowellmo <kpmcdowellmo@gmail.com>
2019-04-26 00:38:58 -0500

    Made a component for the five day forecast real quick

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,13 +5,15 @@ import './index.css';
 import App from './components/App/App';
 import UVIndex from './components/UVIndex/UVIndex';
 import * as serviceWorker from './serviceWorker';
+import FDForecast from './components/FDForecast/FDForecast';
 
 ReactDOM.render(
-<div>
-    <App/>
-    <UVIndex/>
-</div>, 
-document.getElementById('root'));
+    <div>
+        <App />
+        <UVIndex />
+        <FDForecast />
+    </div>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
