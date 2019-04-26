1400e5465e623c518e6a4360ace34589d6289ab7
Daniel <dwc678@live.missouristate.edu>
2019-04-26 16:02:44 -0500

    App.jsx Routing

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import { 
-  Container, 
-  Row, 
-  Col, 
-  Button, 
-  Form, 
-  FormGroup, 
-  Input,  
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Collapse,
   Navbar,
+  NavbarToggler,
   NavbarBrand,
   Nav,
+  NavLink,
   NavItem,
-  NavLink
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
 } from 'reactstrap';
 import './App.css';
+import FiveDay from './components/5-day-weather/5day';
+import UVIndex from "./components/UVIndex/UVIndex";
 
 
 const ApiKey = '304b69dfc8fd594456d6556ba7d5be48';
 const zipcode = '65810';
 const countrycode = 'us';
-const url = 'https://api.openweathermap.org/data/2.5/weather?zip='+ zipcode + ',' + countrycode + '&appid=' + ApiKey;
+const url = 'https://api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',' + countrycode + '&appid=' + ApiKey;
+
 
 class MyComponent extends React.Component {
   constructor(props) {
@@ -27,23 +38,59 @@ class MyComponent extends React.Component {
     this.state = {
       error: null,
       isLoaded: false,
-      items: {}
+      key: ApiKey,
+      zip: zipcode,
+      items: null
     };
+
+    this.changeZip = this.changeZip.bind(this);
+    this.updateData = this.updateData.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
   }
 
+  handleKeyPress(event) {
+    if (event.key === "Enter") {
+      this.updateData(event);
+    }
+  }
 
   componentDidMount() {
-    fetch(url)
-      .then(response => response.json())
+    this.updateData()
+  }
+
+  changeZip(elem) {
+    elem.persist();
+    this.setState(
+      state => ({
+        zip: elem.target.value
+      })
+    );
+  }
+
+  updateData(elem) {
+    this.setState({
+      isLoaded: false,
+      error: null
+    });
+    if (elem) {
+      elem.preventDefault();
+    }
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${this.state.zip},us&appid=${this.state.key}`)
+      .then(response => {
+        return response.json();
+      })
       .then(
         (result) => {
-          let city = result.name;   
-          console.log("City: " + city);
+          console.log(result)
           this.setState({
-             isLoaded: true,
-             items: result.weather
+            isLoaded: true,
+            items: result
           });
         },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
         (error) => {
           this.setState({
             isLoaded: true,
@@ -53,51 +100,52 @@ class MyComponent extends React.Component {
       )
   }
 
-
   render() {
-    const { error, isLoaded, items } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
+
+    return (
+      <Router>
         <Container>
           <Navbar color="light" light expand="lg">
-            <NavbarBrand href="/">The Weather App </NavbarBrand>
+            <NavbarBrand href="/">The Weather App</NavbarBrand>
             <Nav className="ml-auto" navbar>
               <NavItem>
-                <NavLink href="/hourly/">Hourly Forecast</NavLink>
+                <Form inline id="ZipCode-Form" >
+                  <FormGroup>
+                    <Input id="ZipCode-Input"
+                      placeholder="e.g. 65810"
+                      value={this.state.zip}
+                      onChange={this.changeZip}
+                      onKeyPress={this.handleKeyPress}
+                    ></Input>
+                    <Button
+                      type="submit"
+                      onClick={this.updateData}
+                    > Search </Button>
+                  </FormGroup>
+                </Form>
               </NavItem>
               <NavItem>
-                <NavLink href="/forecast/">5 Day Forecast</NavLink>
+                <NavLink><Link to="/">Current Weather</Link></NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="/radar/">Weather Radar</NavLink>
+                <NavLink><Link to="/forecast">5 Day Forecast</Link></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink><Link to="/uv">UV</Link></NavLink>
               </NavItem>
             </Nav>
           </Navbar>
-    
-            <Row className = "cityName">
-              <h1>
-                Current weather for Springfield...
-              </h1>
-          </Row> 
-          <Row className = "weatherVals">
-            <Col>Current Temperature</Col>
-            <Col>High Temp</Col>
-            <Col>Low Temp</Col>
-            <Col>Hummidity</Col>
-        </Row>
-        <Row className = "weatherVals">
-            <Col>69</Col>
-            <Col>69</Col>
-            <Col>69</Col>
-            <Col>69</Col>
-        </Row>
+        
+
+          <Switch>
+            <Route path="/" exact />
+            <Route path="/forecast" component={FiveDay} />
+            <Route path="/UV" component={UVIndex} />
+          </Switch>
         </Container>
-      );
-    }
+      </Router>
+    );
   }
 }
+
 export default MyComponent;
