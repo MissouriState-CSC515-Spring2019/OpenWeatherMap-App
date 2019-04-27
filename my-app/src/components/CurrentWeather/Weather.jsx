e663d728e8bca8fd8703ea8c5d5a9dd5959acaaf
Timothy McCoy <Timothy91@live.missouristate.edu>
2019-04-27 14:15:28 -0500

    got the icons working

--- my-app/src/components/CurrentWeather/Weather.jsx
+++ my-app/src/components/CurrentWeather/Weather.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Container, Row, Col } from 'reactstrap';
-import '/Users/danielcourtney/CSC515/FinalProject/OpenWeatherMap-App/my-app/src/App.jsx';
 
 class Weather extends React.Component {
 
@@ -30,6 +29,46 @@ class Weather extends React.Component {
         return Math.round((parseInt(temp, 10) - 273.15) * (9/5) + 32);
     }
 
+    componentGetIcon(iconId){
+
+        if (iconId > 199 && iconId < 300){
+            //thunderstorm
+            return "11d";
+
+        } else if (iconId > 299 && iconId < 400){
+            // drizzle
+            return "09d";
+
+        } else if (iconId > 499 && iconId < 600){
+            // rain
+            return "10d";
+
+        } else if (iconId > 599 && iconId < 700){
+            // snow
+            return "13d";
+
+        } else if (iconId > 699 && iconId < 800){
+            // atmosphere
+            return "50d";
+
+        } else if (iconId == 800){
+            // clear
+            return "01d";
+
+        }else if (iconId == 801){
+            // some clouds
+            return "02d";
+
+        } else if (iconId == 802){
+            // more clouds
+            return "03d";
+
+        } else {
+            // all the clouds
+            return "04d";
+        }
+    }
+
     render() {
         if (this.state.error) {
             return <div>Error: {this.error.message}</div>;
@@ -42,7 +81,11 @@ class Weather extends React.Component {
                         <Col><h1>Current Weather for: {this.state.items.name}</h1></Col>
                     </Row>
                     <Row>
-                        {/* TIM DO HERE */}
+                        <Col>Testing response data (temp): {this.componentConvertTemp(this.state.items.main.temp)}</Col>
+                        <Col>{this.state.items.weather[0].main}
+                            <img src ={"http://openweathermap.org/img/w/" + this.componentGetIcon(this.state.items.weather[0].id) + ".png"}/>
+                        </Col>
+                        <Col>{this.state.items.weather[0].description}</Col>
                     </Row>
                     <Row className = "weatherTable">
                         <Col>High Temp</Col>
